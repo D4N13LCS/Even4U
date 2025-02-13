6bec0b2d4504ac1172d1b180007baddd0e84b968
CardosoDaniel <danielmazzini6@gmail.com>
2025-02-13 17:36:47 -0300

    Alteração do user no arquivo db.js

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 const pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
+    user: "root",
     password: process.env.MYSQL_ROOT_PASSWORD,
     port: process.env.MYSQL_PORT,
     database: process.env.MYSQL_DATABASE
